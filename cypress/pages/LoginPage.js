bfebc3babcdbbb7108d30888ed8987ed10d445f2
Anik <mostafizurrahman.anik007@gmail.com>
2026-04-28 09:57:03 +0600

    some comments added to login method for better understanding

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -8,20 +8,24 @@ class LoginPage {
     cy.visit("/");
   }
 
-  login(username, password) {
+  login(username, password) { //method which will be called in test runner
     cy.log(`Logging in as ${username}`);
 
-    cy.url({ timeout: 15000 }).should('include', '/realms');
+    cy.url({ timeout: 15000 }).should("include", "/realms");  // validation; this part can be skipped
 
-    cy.location("origin").then((keycloakOrigin) => {
-      cy.origin(keycloakOrigin, { args: { username, password } }, ({ username, password }) => {
-        cy.get('#username').should('be.visible').clear().type(username);
-        cy.get('#password').should('be.visible').clear().type(password);
-        cy.get("#kc-login").should('be.visible').and("not.be.disabled").click();
-      });
+    cy.location("origin").then((keycloakOrigin) => {  // getting the current url origin then target the url where it accessing
+      cy.origin(
+        keycloakOrigin,
+        { args: { username, password } },
+        ({ username, password }) => { //passing variable safely as it can not directly interact inside the origin so passing as argument
+          cy.get("#username").should("be.visible").clear().type(username); //interacting with username field
+          cy.get("#password").should("be.visible").clear().type(password); // interacting with password field
+          cy.get("#kc-login").should("be.visible").and("not.be.disabled").click(); // interacting with login button
+        },
+      );
     });
 
-    cy.url().should('include', '/landing');
+    cy.url().should("include", "/landing"); // validation; this part can be skipped
   }
 
 }
