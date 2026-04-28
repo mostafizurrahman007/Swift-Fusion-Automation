fed7fbb83281757eab988e2f3ace6e446fb28912
Anik <mostafizurrahman.anik007@gmail.com>
2026-04-28 09:58:09 +0600

    updated login method comments

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -2,32 +2,36 @@
 /// <reference types="cypress-xpath" />
 
 class LoginPage {
-
   visit() {
     cy.log("Visiting the application");
     cy.visit("/");
   }
 
-  login(username, password) { //method which will be called in test runner
+  login(username, password) {
+    //method which will be called in test runner
     cy.log(`Logging in as ${username}`);
 
-    cy.url({ timeout: 15000 }).should("include", "/realms");  // validation; this part can be skipped
+    cy.url({ timeout: 15000 }).should("include", "/realms"); // validation; this part can be skipped
 
-    cy.location("origin").then((keycloakOrigin) => {  // getting the current url origin then target the url where it accessing
+    cy.location("origin").then((keycloakOrigin) => {
+      // getting the current url origin then target the url where it accessing the page
       cy.origin(
         keycloakOrigin,
         { args: { username, password } },
-        ({ username, password }) => { //passing variable safely as it can not directly interact inside the origin so passing as argument
+        ({ username, password }) => {
+          //passing variable safely as it can not directly interact inside the origin so passing as argument
           cy.get("#username").should("be.visible").clear().type(username); //interacting with username field
           cy.get("#password").should("be.visible").clear().type(password); // interacting with password field
-          cy.get("#kc-login").should("be.visible").and("not.be.disabled").click(); // interacting with login button
+          cy.get("#kc-login")
+            .should("be.visible")
+            .and("not.be.disabled")
+            .click(); // interacting with login button
         },
       );
     });
 
     cy.url().should("include", "/landing"); // validation; this part can be skipped
   }
-
 }
 
-export default new LoginPage;
+export default new LoginPage();
