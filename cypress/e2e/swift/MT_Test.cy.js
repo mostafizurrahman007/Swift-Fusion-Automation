8e5a7072c9f76ee56d992b527ee9d67ebd191618
Anik <mostafizurrahman.anik007@gmail.com>
2026-05-20 16:01:01 +0600

    Completed MT_700 automation for mandatory field save and authorization operations.

--- cypress/e2e/swift/MT_Test.cy.js
+++ cypress/e2e/swift/MT_Test.cy.js
@@ -8,7 +8,6 @@ import MT_700_Page from "../../pages/swift/MT/MT_700_Page";
 import {
   verifyAlertMessage,
   verifyAuthorization,
-  verifyAndCLoseMandatoryCheckBox,
 } from "../../utils/commonUtils";
 
 describe("Automation Suite for MT Messages", () => {
@@ -33,14 +32,30 @@ describe("Automation Suite for MT Messages", () => {
     // HomePage.searchByFastPath('3728');
     HomePage.search_MT_Message();
     HomePage.goToPage(" MT700 - Issue of a Documentary Credit ");
-    /*MT_700_Page.save();
-        verifyAlertMessage("saved");
-        AuthorizationPage.openMenuAndGoToAuthorization();
-        AuthorizationPage.messageType("MT700");
-        AuthorizationPage.authorizeByBusinessMsgIdentifier(
-          MT_700_Page.documentCreditNumber20,
-        );
-        AuthorizationPage.confirmAuthorization();
-        verifyAuthorization("messages authorized successfully!");*/
+    MT_700_Page.formOfDocumentCredit40A();
+    MT_700_Page.documentCreditNumber20();
+    MT_700_Page.dateOfIssue31C();
+    MT_700_Page.applicableRules40E();
+    MT_700_Page.dateOfExpiry31D();
+    MT_700_Page.placeOfExpiry31D();
+    MT_700_Page.applicantNameAndAddress50();
+    MT_700_Page.beneficiaryNameAndAddress59();
+    MT_700_Page.currencyField();
+    MT_700_Page.currencyAndAmount32B();
+    MT_700_Page.availableWithOption41A();
+    MT_700_Page.availableWithBIC41A();
+    MT_700_Page.availableBy41A();
+    MT_700_Page.confirmInstructions49();
+    MT_700_Page.senderBIC();
+    MT_700_Page.receiverBIC();
+    MT_700_Page.save();
+    verifyAlertMessage("saved");
+    AuthorizationPage.openMenuAndGoToAuthorization();
+    AuthorizationPage.messageType("MT700");
+    AuthorizationPage.authorizeByBusinessMsgIdentifier(
+      MT_700_Page.documentCreditNumber_20,
+    );
+    AuthorizationPage.confirmAuthorization();
+    verifyAuthorization("messages authorized successfully!");
   });
 });
--- cypress/pages/swift/MT/MT_700_Page.js
+++ cypress/pages/swift/MT/MT_700_Page.js
@@ -2,12 +2,172 @@
 /// <reference types="cypress-xpath" />
 
 import { randomNumber } from "../../../utils/commonUtils";
-import { commonLocators } from "../../support/locators/commonLocators";
-import { randomString } from "../../utils/commonUtils";
+import { commonLocators } from "../../../support/locators/commonLocators";
+import {
+  getCurrentDateDDMMYYYY,
+  randomString,
+} from "../../../utils/commonUtils";
 
 class MT_700_Page {
-    documentCreditNumber20 = randomNumber();
+  documentCreditNumber_20 = randomNumber();
 
+  formOfDocumentCredit40A() {
+    commonLocators
+      .ByControlName("docCrForm40A")
+      .first()
+      .should("be.visible")
+      .click();
+    commonLocators
+      .ByTextWithTag("div", " REVOCABLE ")
+      .should("be.visible")
+      .click();
+  }
+
+  documentCreditNumber20() {
+    commonLocators
+      .ByControlName("docCrNum20")
+      .first()
+      .should("be.visible")
+      .clear()
+      .type(this.documentCreditNumber_20);
+  }
+
+  dateOfIssue31C() {
+    commonLocators
+      .ByControlName("issueDt31C")
+      .first()
+      .clear()
+      .type(getCurrentDateDDMMYYYY() + "{enter}");
+  }
+
+  applicableRules40E() {
+    commonLocators
+      .ByControlName("appRules40E")
+      .first()
+      .should("be.visible")
+      .click();
+    commonLocators
+      .ByTextWithTag("div", " UCP LATEST VERSION ")
+      .should("be.visible")
+      .click();
+  }
+
+  dateOfExpiry31D() {
+    commonLocators
+      .ByControlName("expiryDt31D")
+      .first()
+      .clear()
+      .type(getCurrentDateDDMMYYYY() + "{enter}");
+  }
+
+  placeOfExpiry31D() {
+    commonLocators
+      .ByControlName("expiryPlace31D")
+      .first()
+      .should("be.visible")
+      .clear()
+      .type("Dhaka");
+  }
+
+  applicantNameAndAddress50() {
+    commonLocators
+      .ByID("applicant50_line_0")
+      .should("be.visible")
+      .clear()
+      .type("Anik, Dhaka");
+  }
+
+  beneficiaryNameAndAddress59() {
+    commonLocators
+      .ByID("benf59_line_0")
+      .should("be.visible")
+      .clear()
+      .type("Anik, Dhaka");
+  }
+
+  currencyField() {
+    commonLocators.ByControlName("curCode32B").should("be.visible").click();
+    commonLocators
+      .ByTextWithTag("div", " USD - US Dollar ")
+      .should("be.visible")
+      .click();
+  }
+
+  currencyAndAmount32B() {
+    commonLocators
+      .ByControlName("amount32B")
+      .should("be.visible")
+      .clear()
+      .type(5000);
+  }
+
+  availableWithOption41A() {
+    commonLocators
+      .ByControlName("availWithOption41")
+      .first()
+      .should("be.visible")
+      .click();
+    commonLocators
+      .ByTextWithTag("div", " Option A - Identifier Code (BIC) ")
+      .should("be.visible")
+      .click();
+  }
+
+  availableWithBIC41A() {
+    commonLocators
+      .ByControlName("availWithBic41A")
+      .scrollIntoView()
+      .should("be.visible")
+      .clear()
+      .type("SEBDBDDH{enter}");
+  }
+
+  availableBy41A() {
+    commonLocators
+      .ByControlName("availBy41D")
+      .first()
+      .should("be.visible")
+      .click();
+    commonLocators
+      .ByTextWithTag("div", " BY PAYMENT ")
+      .should("be.visible")
+      .click();
+  }
+
+  confirmInstructions49() {
+    commonLocators
+      .ByControlName("confirmation49")
+      .first()
+      .scrollIntoView()
+      .should("be.visible")
+      .click();
+    commonLocators
+      .ByTextWithTag("div", " CONFIRM - Receiver requested to confirm credit ")
+      .should("be.visible")
+      .click();
+  }
+
+  senderBIC() {
+    commonLocators
+      .ByControlName("senderBic")
+      .scrollIntoView()
+      .should("be.visible")
+      .clear()
+      .type("SEBDBDDH{enter}");
+  }
+
+  receiverBIC() {
+    commonLocators
+      .ByControlName("receiverBic")
+      .scrollIntoView()
+      .should("be.visible")
+      .clear()
+      .type("BKBABDDH{enter}");
+  }
+
+  save() {
+    commonLocators.ByTextWithTag("button", "Save").click({ force: true });
+  }
 }
 
 export default new MT_700_Page();
